766516e9546301b2b1bdd8edfcf79e8e340c4e0f
morganherlocker <morgan.herlocker@gmail.com>
2014-12-01 16:24:01 -0500

    basic line building (with some hardcoded values)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var isolines = require('turf-isolines'),
     point = require('turf-point'),
     extent = require('turf-extent'),
     featureCollection = require('turf-featurecollection'),
+    polylineDecode = require('polyline').decode,
     OSRM = require('osrm');
 
 module.exports = function (center, time, resolution, network, done) {
@@ -15,56 +16,72 @@ module.exports = function (center, time, resolution, network, done) {
     var spokes = featureCollection([])
     var miles = time * 1.4;
     spokes.features.push(destination(centerPt, miles, 180, 'miles'));
-    spokes.features.push(destination(centerPt, miles, -180, 'miles'));
+    spokes.features.push(destination(centerPt, miles, 0, 'miles'));
     spokes.features.push(destination(centerPt, miles, 90, 'miles'));
     spokes.features.push(destination(centerPt, miles, -90, 'miles'));
     var bbox = extent(spokes);
 
     //compute destination grid
-    var targets = grid(bbox, 30);
+    var targets = grid(bbox, resolution);
     var routes = featureCollection([]);
     var destinations = featureCollection([]);
     var i = 0;
+    var routedNum = 0;
 
     getNext(i);
 
     function getNext(i){
-      if(i < targets.features.length) {
-        var query = {
-          coordinates: [
-            [
-              center[1], center[0]
-            ],
-            [
-              targets.features[i].geometry.coordinates[1], targets.features[i].geometry.coordinates[0]
-            ]
-          ]
-        };
-        osrm.route(query, function(err, res){
-          if(err) done(err)
-          else if (!res || !res.route_summary.total_time || !res.route_summary.total_time) {
-            return done(new Error('No data found for location: %s', center));
-          } else {
-            destinations.features.push({
-              type: 'Feature',
-              properties: {
-                eta: res.route_summary.total_time,
-                dist: res.route_summary.total_distance
-              },
-              geometry:{
-                type: 'Point',
-                coordinates: [res.via_points[1][1], res.via_points[1][0]]
-              }
-            })
-            routes.features.push(decode(res));
-          }
-          i++;
-          getNext(i);
-        })
-      } else {
-        var line = isolines(destinations, 'eta', 50, [time]);
-        done(null, line);
-      }
+        if(destinations.length >= targets.length){
+            return
+        }
+        if(i < targets.features.length) {
+            var query = {
+                coordinates: [
+                    [
+                      center[1], center[0]
+                    ],
+                    [
+                      targets.features[i].geometry.coordinates[1], targets.features[i].geometry.coordinates[0]
+                    ]
+                ]
+            };
+        
+            osrm.route(query, function(err, res){
+                i++;
+                if(err) console.log(err)
+                if(err) return done(err)
+                else if (!res || !res.route_summary) {
+                    destinations.features.push({
+                        type: 'Feature',
+                        properties: {
+                            eta: 500,
+                            dist: 500
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [query.coordinates[1][1], query.coordinates[1][0]]
+                        }
+                    });
+                } else {
+                    destinations.features.push({
+                        type: 'Feature',
+                        properties: {
+                            eta: res.route_summary.total_time,
+                            dist: res.route_summary.total_distance
+                        },
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [res.via_points[1][1], res.via_points[1][0]]
+                        }
+                        });
+                    routes.features.push(decode(res));
+                }
+                getNext(i);
+            });
+        } else {
+            var line = isolines(destinations, 'eta', resolution * 1.2, [400]);
+            return done(null, line);
+        }
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -3,23 +3,19 @@ var test = require('tape'),
     isochrone = require('../');
 
 test('osrm-isochrone', function(t) {
-    t.plan(7);
-    var resolution = 10;
-    var time = 10;
+    var resolution = 35;
+    var time = 5;
     var network = '../dc.osrm'
     var locations = [
-        [-75.16416549682616,39.94804262581411],
-        [-73.95506858825684,40.714411227628915],
-        [-73.20877075195312,44.461475576425855],
-        [-84.51503276824951,39.09856077249074],
-        [-84.38907623291016,33.761880986766215],
-        [-80.20706176757812,25.784435164398015],
-        [-118.3612060546875,33.98436372829188]
+        [-77.02926635742188,38.90011780426885]
     ];
 
     locations.forEach(function(location) {
-        var drivetime = isochrone(location, time, resolution, network, function(err, res) {
+        isochrone(location, time, resolution, network, function(err, drivetime) {
+            if(err) throw err;
             t.ok(drivetime);
+            console.log(JSON.stringify(drivetime))
+            t.end()
         });
     });
 });
