48d34d2cf1babab731a351543e0d22943777f811
Fabien Girard <fabien@mapotempo.com>
2015-10-02 16:17:15 +0200

    Clean code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = function (center, time, resolution, network, done) {
     // bbox should go out 1.4 miles in each direction for each minute
     // this will account for a driver going a bit above the max safe speed
     var centerPt = point(center[0], center[1]);
-    var spokes = featureCollection([])
+    var spokes = featureCollection([]);
     var miles = (time/60) * 1.2; // assume 70mph max speed
     spokes.features.push(destination(centerPt, miles, 180, 'miles'));
     spokes.features.push(destination(centerPt, miles, 0, 'miles'));
@@ -23,7 +23,6 @@ module.exports = function (center, time, resolution, network, done) {
 
     //compute destination grid
     var targets = grid(bbox, resolution);
-    var routes = featureCollection([]);
     var destinations = featureCollection([]);
     var i = 0;
     var routedNum = 0;
@@ -32,7 +31,7 @@ module.exports = function (center, time, resolution, network, done) {
 
     function getNext(i){
         if(destinations.length >= targets.length){
-            return
+            return;
         }
         if(i < targets.features.length) {
             var query = {
@@ -48,8 +47,8 @@ module.exports = function (center, time, resolution, network, done) {
         
             osrm.route(query, function(err, res){
                 i++;
-                if(err) console.log(err)
-                if(err) return done(err)
+                if(err) console.log(err);
+                if(err) return done(err);
                 else if (!res || !res.route_summary) {
                     destinations.features.push({
                         type: 'Feature',
@@ -73,8 +72,7 @@ module.exports = function (center, time, resolution, network, done) {
                             type: 'Point',
                             coordinates: [res.via_points[1][1], res.via_points[1][0]]
                         }
-                        });
-                    routes.features.push(decode(res));
+                    });
                 }
                 getNext(i);
             });
@@ -83,24 +81,4 @@ module.exports = function (center, time, resolution, network, done) {
             return done(null, line);
         }
     }
-}
-
-function decode (res) {
-    var route = {
-        type: 'Feature',
-        geometry: {
-            type: 'LineString',
-            coordinates: polylineDecode(res.route_geometry)
-        },
-        properties: {
-            eta: res.route_summary.total_time,
-            dist: res.route_summary.total_distance
-        }
-    };
-    route.geometry.coordinates = route.geometry.coordinates.map(function(c){
-        var lon = c[1] * 0.1;
-        var lat = c[0] * 0.1;
-        return [lon, lat];
-    });
-    return route;
 }
