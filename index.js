86d48b61e5c9838a8d041e9e4f24fcf06dc5ce53
Francisco Valdez de la Fuente <pacofvf@gmail.com>
2016-07-26 13:30:40 -0500

    add debug logs

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ module.exports = function (center, time, options, done) {
                     res.durations[0].length == res.destinations.length) {
 
                     res.durations[0].forEach(function(time, idx) {
+                        console.log([time, idx]);
                         var distanceMapped = distance(
                             point(coord[idx][0], coord[idx][1]),
                             point(res.destinations[idx][0], res.destinations[idx][1]),
